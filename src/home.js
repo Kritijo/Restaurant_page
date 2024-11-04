0f7747e988d6a5b58e5deb989328b18400fd5ce4
KRITI JOSHI <kritijoshi@KRITIs-MacBook-Pro.local>
2024-11-05 02:04:33 +0530

    Populate content section with home tab

---
+++ src/home.js
@@ -0,0 +1,13 @@
+export function display(){ 
+    let content = document.querySelector("#content");
+    content.textContent = '';
+
+    content.classList.add("home");
+    let img = document.createElement("div");
+    img.classList.add("homeimg");
+
+    let para = document.createElement("p");
+    para.textContent = "Welcome to Firenze, where we serve delicious artisan dishes made with fresh, locally-sourced ingredients. Join us for an unforgettable dining experience filled with flavor and warmth. We can't wait to see you!";
+
+    content.append(img,para);
+}
